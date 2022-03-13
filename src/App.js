633179c9dc5999ece50b918c0a9aff2bee3e456a
tatyanastarovoyt <101508162+tatyanastarovoyt@users.noreply.github.com>
2022-03-13 15:11:53 +0200

    Update App.js
    google

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React AAAAAAA
+          Learn goole
         </a>
       </header>
     </div>
