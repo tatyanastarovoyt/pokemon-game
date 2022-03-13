80757230eb1c54420970ea2f9d3894573ea64a73
tatyanastarovoyt <101508162+tatyanastarovoyt@users.noreply.github.com>
2022-03-13 15:03:05 +0200

    change me

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
          Hello World!!!
         </p> */}
         <p>
-          Hello Vinnitsa!!!
+          Hi everyone!
         </p>
         <a
           className="App-link"
