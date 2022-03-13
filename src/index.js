0ced02f58c59c3fc7840ad292c5e3073acaa9657
tatyanastarovoyt <101508162+tatyanastarovoyt@users.noreply.github.com>
2022-03-13 20:49:59 +0200

    img

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ const AppList = () => {
   )
 }
 
-const AppHeader = () => {
-  return <h2>Hello World!!!!!!</h2>
+const Image = () => {
+  return <img src="https://www.theknot.com/tk-media/images/8d7f9239-ddb5-47e7-8c81-eb39283c8262~rs_768.h" alt="IMAGE NULL" width="200" height="150s"/>
 }
+
+const AppHeader = () => <h2>Hello World!!!!!!</h2>
 
 const App = () => {
   return <div>
+    {/* <Image /> */}
     <AppHeader />
     <AppList />
+    <Image />
+
   </div>
 }
 
